9ef04b23bc76915c4975a05f7d8898a99df83c25
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-02-12 10:48:33 +0100

    Alpine upgraded from 3.16 to 3.17

--- shell/.welcome.sh
+++ shell/.welcome.sh
@@ -2,5 +2,5 @@ echo "Terminal Go tools:"
 echo " * mockgen: generate mocks and stubs"
 echo " * mockery: generate testify/mock mocks from interfaces"
 echo "Terminal Docker tools aliases:"
-echo " * alpine: launch an interactive alpine 3.16 container"
+echo " * alpine: launch an interactive alpine 3.17 container"
 
