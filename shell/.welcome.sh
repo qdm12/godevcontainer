48719e3b3834b938cd7436ce7b731e320aa55144
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-10-10 21:15:14 +0200

    alpine upgraded from 3.19 to 3.20

--- shell/.welcome.sh
+++ shell/.welcome.sh
@@ -2,5 +2,5 @@ echo "Terminal Go tools:"
 echo " * mockgen: generate mocks and stubs"
 echo " * mockery: generate testify/mock mocks from interfaces"
 echo "Terminal Docker tools aliases:"
-echo " * alpine: launch an interactive alpine 3.19 container"
+echo " * alpine: launch an interactive alpine 3.20 container"
 
